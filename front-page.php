3605d0c323fb8037c34247721a2e2ec2b9cc965e
0legPr <oleg2007proyavko@gmail.com>
2022-11-01 19:23:45 +0200

    main block

--- front-page.php
+++ front-page.php
@@ -6,5 +6,7 @@ Template Name: Home
 <?php
 get_header(); ?>
 
+<?php get_template_part('template-parts/main-block'); ?>
+
 <?php
 get_footer(); ?>
---
+++ template-parts/main-block.php
@@ -0,0 +1,12 @@
+<div class="main-block" style="background-image: url('<?php echo get_template_directory_uri(); ?>/src/img/main-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-6">
+                <h1>Демонтаж конструкцій та споруд</h1>
+            </div>
+            <div class="col-md-6 col-lg-2">
+                <form action="#">form</form>
+            </div>
+        </div>
+    </div> 
+</div>
